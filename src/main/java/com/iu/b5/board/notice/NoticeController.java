ae3ec43e44ac45d3e037938975849d87701424bf
Daeki <kimdaeki@gmail.com>
2021-11-08 14:06:55 +0900

    1

--- src/main/java/com/iu/b5/board/notice/NoticeController.java
+++ src/main/java/com/iu/b5/board/notice/NoticeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.iu.b5.board.BoardService;
 import com.iu.b5.board.BoardVO;
 import com.iu.b5.util.Pager;
 
